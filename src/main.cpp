bdd4a4b3d152eef8270d01b25795129496dbcae8
PascalSkylake <jackcammarata1@gmail.com>
2025-04-02 13:57:13 -0400

    ermmm

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 OtosDevice otos;
 
-enum UARTMODE { TX = 0, RX = 1 };
+enum UARTMODE { TX = 1, RX = 0 };
 
 void set_uart_mode(uint gpio, UARTMODE mode) {
     gpio_put(gpio, mode);
@@ -55,6 +55,7 @@ size_t cobsEncode(const void *data, size_t length, uint8_t *buffer)
 size_t cobsDecode(const uint8_t *buffer, size_t length, void *data)
 {
 	assert(buffer && data);
+    // printf("decoding\n");
 
 	const uint8_t *byte = buffer; // Encoded input byte pointer
 	uint8_t *decode = (uint8_t *)data; // Decoded output byte pointer
@@ -94,18 +95,18 @@ int main() {
 
     printf("basic readings\n");
 
-    while (otos.begin() == false) {
-        printf("otos not connected, check wiring and i2c address\n");
-        sleep_ms(1);
-    }
+    // while (otos.begin() == false) {
+    //     printf("otos not connected, check wiring and i2c address\n");
+    //     sleep_ms(1);
+    // }
 
-    printf("otos connected\n");
+    // printf("otos connected\n");
 
 
-    otos.set_linear_unit(kInches);
-    otos.set_angular_unit(kDegrees);
+    // otos.set_linear_unit(kInches);
+    // otos.set_angular_unit(kDegrees);
 
-    otos.calibrate_imu();
+    // otos.calibrate_imu();
 
     gpio_set_function(0, UART_FUNCSEL_NUM(uart0, 0));
     gpio_set_function(1, UART_FUNCSEL_NUM(uart0, 1));
@@ -116,26 +117,37 @@ int main() {
     set_uart_mode(2, RX);
 
     uart_init(uart0, 115200);
+    uint8_t data[1] = {0b00010101};
+
+    while (true) {
+        // uart_write_blocking(uart0, data, 1);
+        sleep_ms(10);
+    }
+    // while (!uart_is_readable(uart0)) {
+    //     printf("asdfaf\n");
+    //     sleep_ms(100);
+    // }
 
     uint8_t ctl[1];
-    uint8_t ctl_cobs[2];
+    uint8_t ctl_cobs[3];
 
     uint8_t config[6];
-    uint8_t config_cobs[7];
+    uint8_t config_cobs[8];
 
     uint8_t calib_samples[1];
-    uint8_t calib_samples_cobs[2];
+    uint8_t calib_samples_cobs[3];
 
     uint8_t linear_scalar[4];
-    uint8_t linear_scalar_cobs[5];
+    uint8_t linear_scalar_cobs[6];
 
     uint8_t angular_scalar[4];
-    uint8_t angular_scalar_cobs[5];
+    uint8_t angular_scalar_cobs[6];
 
     uint8_t success[1];
-    uint8_t success_cobs[2];
+    uint8_t success_cobs[3];
     *success = 0xFF;
     cobsEncode(success, sizeof(success), success_cobs);
+    success_cobs[2] = 0x00;
 
     uint8_t failure[1];
     uint8_t failure_cobs[2];
@@ -143,16 +155,25 @@ int main() {
     cobsEncode(failure, sizeof(failure), failure_cobs);
 
     uint8_t forty_eight_bit[6];
-    uint8_t forty_eight_bit_cobs[7];
+    uint8_t forty_eight_bit_cobs[8];
 
     uint8_t ninety_six_bit[12];
-    uint8_t ninety_six_bit_cobs[13];
+    uint8_t ninety_six_bit_cobs[14];
 
     uint8_t one_forty_four[18];
-    uint8_t one_forty_four_cobs[19];
+    uint8_t one_forty_four_cobs[20];
 
     otos_pose2d_t config_pose;
 
+    // while (true) {
+    //     if (uart_is_readable(uart0)) {
+    //         uint8_t jawn[1];
+    //         uart_read_blocking(uart0, jawn, 1);
+    //         printf("%X \n", jawn[0]);
+    //     }
+    //     sleep_ms(1);
+    // }
+    
 
     while (true) {
         // await packet from controller
@@ -172,41 +193,57 @@ int main() {
         // 0b10001000 is set offset (wait for another 48 bit packet with the offset to set) respond 0xFF when finished
         // anything else is fake as hell respond 0xF0
         // after sending response, switch back to rx mode
+        // while (!uart_is_readable(uart0)) {
+        //     printf("bad\n");
+        //     sleep_ms(500);
+        // }
+        // printf("good\n");
         
         uart_read_blocking(uart0, ctl_cobs, sizeof(ctl_cobs));
         cobsDecode(ctl_cobs, sizeof(ctl_cobs), ctl);
+
+        // for (int i = 0; i < sizeof(ctl_cobs))
+
+        printf("BYTE!! %X \n", *ctl);
         
 
         // if it's a config packet
-        if (*ctl & 0b00001000) {
+        if (*ctl == 0b00001000) {
+            printf("config\n");
             uart_read_blocking(uart0, config_cobs, sizeof(config_cobs));
             cobsDecode(config_cobs, sizeof(config_cobs), config);
 
             otos.regs_to_pose(config, config_pose, INT16_TO_METER, INT16_TO_RAD);
             otos.set_position(config_pose);
 
+            printf("setting position %f, %f, %f\n", config_pose.x, config_pose.y, config_pose.h);
+
             set_uart_mode(2, TX);
+            sleep_ms(100);
             uart_write_blocking(uart0, success_cobs, sizeof(success_cobs));
-            uart_putc(uart0, 0);
+            printf("sent: %X %X %X\n", success_cobs[0], success_cobs[1], success_cobs[2]);
         } // if it's a calibrate packet
-        else if (*ctl & 0b00010000) {
+        else if (*ctl == 0b00010000) {
+            printf("calibrate\n");
             uart_read_blocking(uart0, calib_samples_cobs, sizeof(calib_samples_cobs));
             cobsDecode(calib_samples_cobs, sizeof(calib_samples_cobs), calib_samples);
 
             otos.calibrate_imu(*calib_samples, true);
 
             set_uart_mode(2, TX);
+            sleep_ms(10);
             uart_write_blocking(uart0, success_cobs, sizeof(success_cobs));
-            uart_putc(uart0, 0);
         } // if it's a reset packet
-        else if (*ctl & 0b00100000) {
+        else if (*ctl == 0b00100000) {
+            printf("reset\n");
             otos.reset_tracking();
 
             set_uart_mode(2, TX);
+            sleep_ms(10);
             uart_write_blocking(uart0, success_cobs, sizeof(success_cobs));
-            uart_putc(uart0, 0);
         } // if it's a set linear scalar packet
-        else if (*ctl & 0b01000000) {
+        else if (*ctl == 0b01000000) {
+            printf("linearscalar\n");
             uart_read_blocking(uart0, linear_scalar_cobs, sizeof(linear_scalar_cobs));
             cobsDecode(linear_scalar_cobs, sizeof(linear_scalar_cobs), linear_scalar);
 
@@ -216,10 +253,11 @@ int main() {
             otos.set_linear_scalar(linear_scalarf);
 
             set_uart_mode(2, TX);
+            sleep_ms(10);
             uart_write_blocking(uart0, success_cobs, sizeof(success_cobs));
-            uart_putc(uart0, 0);
         } // if it's a set angular scalar packet
-        else if (*ctl & 0b10000000) {
+        else if (*ctl == 0b10000000) {
+            printf("angularscalar\n");
             uart_read_blocking(uart0, angular_scalar_cobs, sizeof(angular_scalar_cobs));
             cobsDecode(angular_scalar_cobs, sizeof(angular_scalar_cobs), angular_scalar);
 
@@ -229,55 +267,68 @@ int main() {
             otos.set_angular_scalar(angular_scalarf);
 
             set_uart_mode(2, TX);
+            sleep_ms(10);
             uart_write_blocking(uart0, success_cobs, sizeof(success_cobs));
-            uart_putc(uart0, 0);
         } // if it's a set offset packet
-        else if (*ctl & 0b10001000) {
+        else if (*ctl == 0b10001000) {
+            printf("offset\n");
             uart_read_blocking(uart0, config_cobs, sizeof(config_cobs));
             cobsDecode(config_cobs, sizeof(config_cobs), config);
 
             otos.regs_to_pose(config, config_pose, INT16_TO_METER, INT16_TO_RAD);
             otos.set_offset(config_pose);
 
+            printf("setting offset %f, %f, %f\n", config_pose.x, config_pose.y, config_pose.h);
+
             set_uart_mode(2, TX);
+            sleep_ms(10);
             uart_write_blocking(uart0, success_cobs, sizeof(success_cobs));
-            uart_putc(uart0, 0);
+            printf("sent: %X %X %X\n", success_cobs[0], success_cobs[1], success_cobs[2]);
         } // if it's a data request packet
         else if (*ctl & 0b00000111) {
-            if (*ctl & 0b00000001 || *ctl & 0b00000010 || *ctl & 0b00000100) {
+            printf("data\n");
+            if (*ctl == 0b00000001 || *ctl == 0b00000010 || *ctl == 0b00000100) {
                 int16_t ints[3];
                 // only one of pos vel acc
-                if (*ctl & 0b00000001) {
+                if (*ctl == 0b00000001) {
+                    printf("pos\n");
                     otos.get_pos_raw(ints[0], ints[1], ints[2]);
-                } else if (*ctl & 0b00000010) {
+                } else if (*ctl == 0b00000010) {
+                    printf("vel\n");
                     otos.get_vel_raw(ints[0], ints[1], ints[2]);
-                } else if (*ctl & 0b00000100) {
+                } else if (*ctl == 0b00000100) {
+                    printf("acc\n");
                     otos.get_acc_raw(ints[0], ints[1], ints[2]);
                 }
                 set_uart_mode(2, TX);
+                sleep_ms(10);
 
                 memcpy(&forty_eight_bit[0], &ints[0], sizeof(ints[0]));
                 memcpy(&forty_eight_bit[2], &ints[1], sizeof(ints[1]));
                 memcpy(&forty_eight_bit[4], &ints[2], sizeof(ints[2]));
                 cobsEncode(forty_eight_bit, sizeof(forty_eight_bit), forty_eight_bit_cobs);
+                forty_eight_bit_cobs[7] = 0x00;
 
                 uart_write_blocking(uart0, forty_eight_bit_cobs, sizeof(forty_eight_bit_cobs));
-                uart_putc(uart0, 0);
-
-            } else if (*ctl & 0b00000011 || *ctl & 0b00000101 || *ctl & 0b00000110) {
+                printf("sent: %X %X %X %X %X %X %X %X\n", forty_eight_bit_cobs[0], forty_eight_bit_cobs[1], forty_eight_bit_cobs[2], forty_eight_bit_cobs[3], forty_eight_bit_cobs[4], forty_eight_bit_cobs[5], forty_eight_bit_cobs[6], forty_eight_bit_cobs[7]);
+            } else if (*ctl == 0b00000011 || *ctl == 0b00000101 || *ctl == 0b00000110) {
                 int16_t ints[6];
                 // which of the options (this is abysmal truly scary and evil)
-                if (*ctl & 0b00000011) {
+                if (*ctl == 0b00000011) {
+                    printf("posvel\n");
                     otos.get_pos_raw(ints[0], ints[1], ints[2]);
                     otos.get_vel_raw(ints[3], ints[4], ints[5]);
-                } else if (*ctl & 0b00000101) {
+                } else if (*ctl == 0b00000101) {
+                    printf("posacc\n");
                     otos.get_pos_raw(ints[0], ints[1], ints[2]);
                     otos.get_acc_raw(ints[3], ints[4], ints[5]);
-                } else if (*ctl & 0b00000110) {
+                } else if (*ctl == 0b00000110) {
+                    printf("velacc\n");
                     otos.get_vel_raw(ints[0], ints[1], ints[2]);
                     otos.get_acc_raw(ints[3], ints[4], ints[5]);
                 }
                 set_uart_mode(2, TX);
+                sleep_ms(10);
 
                 memcpy(&ninety_six_bit[0], &ints[0], sizeof(ints[0]));
                 memcpy(&ninety_six_bit[2], &ints[1], sizeof(ints[1]));
@@ -286,11 +337,13 @@ int main() {
                 memcpy(&ninety_six_bit[8], &ints[4], sizeof(ints[4]));
                 memcpy(&ninety_six_bit[10], &ints[5], sizeof(ints[5]));
                 cobsEncode(ninety_six_bit, sizeof(ninety_six_bit), ninety_six_bit_cobs);
+                ninety_six_bit_cobs[13] = 0x00;
 
                 uart_write_blocking(uart0, ninety_six_bit_cobs, sizeof(ninety_six_bit_cobs));
                 uart_putc(uart0, 0);
 
-            } else if (*ctl & 0b00000111) {
+            } else if (*ctl == 0b00000111) {
+                printf("all\n");
                 int16_t ints[9];
                 // all of pos vel acc
                 otos.get_pos_raw(ints[0], ints[1], ints[2]);
@@ -298,6 +351,7 @@ int main() {
                 otos.get_acc_raw(ints[6], ints[7], ints[8]);
 
                 set_uart_mode(2, TX);
+                sleep_ms(10);
 
                 
                 memcpy(&one_forty_four[0], &ints[0], sizeof(ints[0]));
@@ -310,14 +364,17 @@ int main() {
                 memcpy(&one_forty_four[14], &ints[7], sizeof(ints[0]));
                 memcpy(&one_forty_four[16], &ints[8], sizeof(ints[0]));
                 cobsEncode(one_forty_four, sizeof(one_forty_four), one_forty_four_cobs);
+                one_forty_four_cobs[19] = 0x00;
 
                 uart_write_blocking(uart0, one_forty_four_cobs, sizeof(one_forty_four_cobs));
                 uart_putc(uart0, 0);
 
 
             } else {
+                printf("ugh\n");
                 // if this happens I'm quitting rit vecks you robotics
                 set_uart_mode(2, TX);
+                sleep_ms(10);
                 uart_write_blocking(uart0, failure_cobs, sizeof(failure_cobs));
                 uart_putc(uart0, 0);
             }
@@ -325,10 +382,14 @@ int main() {
 
         } // if it's a fake ass packet
         else {
+            printf("idiot\n");
             set_uart_mode(2, TX);
+            sleep_ms(10);
             uart_write_blocking(uart0, failure_cobs, sizeof(failure_cobs));
             uart_putc(uart0, 0);
         }
+        sleep_ms(5);
+        printf("done processing\n");
 
         set_uart_mode(2, RX);
         
